a3e36ab71b744d4ff3223b4ec4e5eb55f6825e58
Zapodask <joaopedroemerim@gmail.com>
2021-01-20 02:23:11 -0300

    .

--- src/styles/components/form/person.ts
+++ src/styles/components/form/person.ts
@@ -4,5 +4,8 @@ export const Container = styled.div`
 `
 
 export const Box = styled.div`
-
+    Input: {
+        border: 1px solid black;
+        border-radius: 5px;
+    }
 `
---
+++ src/styles/components/input.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const InputS = styled.input`
+    border: 1px solid black;
+    border-radius: 5px;
+`
